3393a33a619e8a5b7190c30ebaa2c344f826c427
Jocelyn Naarden <tzionone@gmail.com>
2022-03-21 21:21:42 -0400

    ✨[Scope Lab [complete]✨

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(name) {
+  bestCustomer = name
+}
+
+const leastFavoriteCustomer = 'Juice'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Orange"
+}
